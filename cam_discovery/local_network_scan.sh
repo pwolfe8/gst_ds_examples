e7698550e888ac410a13682286abc906ec841173
Philip Wolfe <pwolfe854@gmail.com>
2022-01-25 19:04:41 -0500

    updated local network scan and gitignore

--- cam_discovery/local_network_scan.sh
+++ cam_discovery/local_network_scan.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
-SUBNET=${1:-10.80.14.172/24}
+SUBNET=$1
+if [ -z "$SUBNET" ]; then
+  echo "please specify subnet to scan as arg."
+  echo "example: 192.168.0.1/24"
+  echo "alternatively if you want to scan your default interface whole local network subnet run:"
+  echo "sudo arp-scan -l"
+fi
 
 # install prereqs if needed
 if [ !  -x "$(command -v nmap)" ]; then
@@ -13,4 +19,6 @@ fi
 sudo nmap -sn $SUBNET
 
 # now run arp-scan to gather local mac address info
-sudo arp-scan -l | tee >(awk '(NR>2) {print $1,"\t\t",$2,"\t\t",substr($0,index($0,$3))}'| head -n-3 > local_network_out.txt) 
+# sudo arp-scan -l | tee >(awk '(NR>2) {print $1,"\t\t",$2,"\t\t",substr($0,index($0,$3))}'| head -n-3 > local_network_out.txt) 
+# or run subnet only 
+sudo arp-scan $SUBNET | tee >(awk '(NR>2) {print $1,"\t\t",$2,"\t\t",substr($0,index($0,$3))}'| head -n-3 > local_network_out.txt) 
