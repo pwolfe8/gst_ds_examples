b658c78fdcf0d588500a23ef0928558e9ef564c1
Philip Wolfe <pwolfe854@gmail.com>
2022-02-15 11:05:48 -0500

    updates to rtsp url testing

--- cam_discovery/record_rtsp_url_test.sh
+++ cam_discovery/record_rtsp_url_test.sh
@@ -2,14 +2,14 @@
 RTSP_URL=${1:-rtsp://admin:admin@10.219.189.164/cam/realmonitor?channel=1&subtype=1}
 # gst-launch-1.0 -e rtspsrc location=$RTSP_URL latency=500 ! decodebin ! x264enc ! matroskamux ! filesink location=filename_h264.mkv
 
-DEBUG_TEMP_DIR=./debug_rtsp_test
-rm -r $DEBUG_TEMP_DIR
+DEBUG_TEMP_DIR="./debug_rtsp_test"
+sudo rm -rf $DEBUG_TEMP_DIR
 mkdir -p $DEBUG_TEMP_DIR
 sleep 0.1
 
-GST_DEBUG=1 \
+sudo GST_DEBUG=1 \
 GST_DEBUG_DUMP_DOT_DIR=$DEBUG_TEMP_DIR \
-gst-launch-1.0 -e rtspsrc location=$RTSP_URL latency=500 ! rtph264depay ! avdec_h264 ! matroskamux ! filesink location=test_record.mkv
+gst-launch-1.0 -e rtspsrc location=$RTSP_URL latency=500 ! rtph264depay ! avdec_h264 ! x264enc ! matroskamux ! filesink location=test_record.mkv
 # gst-launch-1.0 -e rtspsrc location=$RTSP_URL latency=500 ! decodebin ! matroskamux ! filesink location=filename_h264.mkv
 
 dot2graph $DEBUG_TEMP_DIR pdf
