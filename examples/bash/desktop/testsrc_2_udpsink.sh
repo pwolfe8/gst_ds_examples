86b5b64d20e336e07b7f1e691f7206b907767e3c
Philip Wolfe <pwolfe854@gmail.com>
2022-02-23 18:22:50 -0500

    added rtsp stuff

--- examples/bash/desktop/testsrc_2_udpsink.sh
+++ examples/bash/desktop/testsrc_2_udpsink.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
 
+INPUT_W=2304
+INPUT_H=1296
+INPUT_FPS=25
+INPUT_CAPS="video/x-raw,format=I420, width=$INPUT_W, height=$INPUT_H, framerate=$INPUT_FPS/1"
+RTSPURL="rtsp://admin:reolink@192.168.0.4//h264Preview_01_main"
+RTSPSRC="rtspsrc latency=1000 location=$RTSPURL ! application/x-rtp,media=video ! rtph264depay ! avdec_h264 ! videoconvert ! $INPUT_CAPS"
+
 TESTSRC="videotestsrc ! video/x-raw,format=I420,width=1920,height=1080,fps=25/1"
 CONVERT2NVMM="nvvideoconvert ! video/x-raw(memory:NVMM)"
 H264="nvv4l2h264enc ! h264parse ! rtph264pay config-interval=10 pt=96"
